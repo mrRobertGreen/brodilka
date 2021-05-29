887e11e0799837ba6d62b82b872f8caf56cb9e62
Robert Green <mrrobertgreen17@gmail.com>
2021-05-29 13:16:25 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const randomBtn = document.querySelector(".random-btn")
+
+    randomBtn?.addEventListener("click", () => {
+        document.location.href = LINKS[getRandomInt(5)]
+    })
+
+
+    const linkElems = document.querySelectorAll("a.row-item")
+    linkElems.forEach((link, idx) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault()
+            const timeout = setTimeout(() => {
+                document.location.href = link.href
+            }, 3000)
+            linkElems.forEach((link, idx) => {
+                if (idx === 1) {
+                    link.style.animation = `go-top 3.5s ease`
+                } else {
+                    link.style.animation = `go-down 3.5s ease`
+                }
+            })
+        })
+    })
+});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+const LINKS = [
+    "red.html",
+    "green.html",
+    "yellow.html",
+    "blue.html",
+    "pink.html",
+]
